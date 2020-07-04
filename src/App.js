12abbd463eeb6f1be5cbaa2998a7df664e16d1a8
Ariel Garcia <ga.arielgarcia@gmail.com>
2020-07-04 13:33:52 -0300

    Primeros ajustes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//API = http://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,ar&APPID=0bdb1e207c717c85595c5fefa172e13d
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Clima</h1>
   );
 }
 
