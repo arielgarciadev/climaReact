8df34a8d36384a22ac8169756e9b52fae2aaa19a
Ariel Garcia <ga.arielgarcia@gmail.com>
2020-07-08 11:07:57 -0300

    Estilos y componente Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-
+import React, {Fragment} from 'react';
+import Header from './components/Header'
 //API = http://api.openweathermap.org/data/2.5/weather?q=Buenos%20Aires,ar&APPID=0bdb1e207c717c85595c5fefa172e13d
 
 function App() {
   return (
-    <h1>Clima</h1>
+    <Fragment>
+      <Header 
+        titulo= 'Clima React App'
+      />
+    </Fragment>
   );
 }
 
