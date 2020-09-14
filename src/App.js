dff1c0b28787c84c151fdb268419fe18754a4506
Ariel Garcia <ga.arielgarcia@gmail.com>
2020-09-14 16:10:17 -0300

    Verificando resultados y manejando errores

--- src/App.js
+++ src/App.js
@@ -1,39 +1,60 @@
 import React, {Fragment, useState, useEffect} from 'react';
-import Header from './components/Header'
-import Formulario from './components/Formulario'
-import Clima from './components/Clima'
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
 
   const [busqueda, guardarBusqueda] = useState({
     ciudad:'',
     pais:''
-});
+  });
 
-const [consultar, guardarConsultar] = useState(false);
-const [resultado, guardarResultado] = useState({});
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
 
 
-const {ciudad, pais} = busqueda;
+  const {ciudad, pais} = busqueda;
 
-useEffect(() => {
-  const consultarAPI = async () => {
-    if (consultar) {
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
 
-      const appId = '0bdb1e207c717c85595c5fefa172e13d';
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&APPID=${appId}`
-  
-      const respuesta = await fetch(url);
-      const resultado = await respuesta.json();
-  
-      guardarResultado(resultado);
-      guardarConsultar(false);
+        const appId = '0bdb1e207c717c85595c5fefa172e13d';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&APPID=${appId}`
+    
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+    
+        guardarResultado(resultado);
+        guardarConsultar(false);
 
+        // Detecta si hubo resultados correctos en la consulta.
+        if (resultado.cod === '404') {
+          guardarError(true)
+        } else {
+          guardarError(false)
+        }
+
+      }
     }
-  }
-  consultarAPI();
+    consultarAPI();
 
-}, [consultar]);
+  }, [consultar]);
+
+  let componente;
+
+  if(error) {
+    componente = <Error mensaje="No hay resultados"/>
+  } else {
+    componente = <Clima
+                    resultado={resultado}
+                  />
+  }
+
+
 
   return (
     <Fragment>
@@ -51,9 +72,7 @@ useEffect(() => {
               />
             </div>
             <div className="col m6 s12">
-              <Clima
-                resultado={resultado}
-              />
+              {componente}
             </div>
           </div>
         </div>
